f7213dd7737e27c2deceea92e3e5ba97d7204d39
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-06-11 17:41:06 +0200

    Initial commit

---
+++ src/main/java/com/libraryproject/homemanagementlibraryproject/HomeManagementLibraryProjectApplication.java
@@ -0,0 +1,13 @@
+package com.libraryproject.homemanagementlibraryproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeManagementLibraryProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeManagementLibraryProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/libraryproject/homemanagementlibraryproject/HomeManagementLibraryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libraryproject.homemanagementlibraryproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeManagementLibraryProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
