7deb3e248da98ecc6a0c8c7605a3e44254cf0092
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-06-12 11:54:00 +0200

    Create Book controller

--- src/main/java/com/libraryproject/homemanagementlibraryproject/controller/impl/BookControllerImpl.java
+++ src/main/java/com/libraryproject/homemanagementlibraryproject/controller/impl/BookControllerImpl.java
@@ -1,7 +1,40 @@
 package com.libraryproject.homemanagementlibraryproject.controller.impl;
 
 import com.libraryproject.homemanagementlibraryproject.controller.BookController;
+import com.libraryproject.homemanagementlibraryproject.entity.BookEntity;
+import com.libraryproject.homemanagementlibraryproject.mapper.BookMapper;
+import com.libraryproject.homemanagementlibraryproject.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
-
+@Controller
 public class BookControllerImpl implements BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @Autowired
+    private BookMapper bookMapper;
+
+    @GetMapping("/books")
+    public String getAllBooks (Model model){
+        model.addAttribute("getAllBooks",bookService.getAllBooks());
+        return "books";
+    }
+
+    public String showNewBookForm (Model model) {
+        BookEntity book = new BookEntity();
+        model.addAttribute("book",book);
+        return "new_book";
+    }
+
+    @PostMapping("/save_book")
+    public String saveBook (@ModelAttribute ("book") BookEntity bookEntity) {
+
+        bookMapper.
+    }
 }
