e72ff4ca943ebe0c2be37f03ac34abdbab611621
Radoslaw Wojtysiak <radoslaw.wojtysiak@allegro.pl>
2021-06-13 11:01:23 +0200

    DB-1 adding books table

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Header from './containers/Header';
 import Home from './containers/Home';
 import Footer from './containers/Footer';
+import Books from "./modules/books/Books";
 
 const App = () => (
     <React.Fragment>
         <Header />
         <Home>
-            Hello Home
+            <Books />
         </Home>
         <Footer />
     </React.Fragment>
---
+++ frontend/src/modules/books/Books.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Books = () => (
+    <table>
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Status</th>
+                <th>Description</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Testowa</td>
+                <td>Testowy</td>
+                <td>OK</td>
+                <td>Testowy opis</td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+)
+
+export default Books;
