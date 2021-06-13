d8ed7d752367fe9cd13716fcfa4a65829e76e488
Radoslaw Wojtysiak <radoslaw.wojtysiak@allegro.pl>
2021-06-13 10:51:50 +0200

    DB-1 adding logo and header content

--- frontend/src/containers/Header.js
+++ frontend/src/containers/Header.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const Header = () => {
-    return 'hello header';
-}
+import './Header.css';
+
+const Header = () => (
+    <header>
+        <img alt="logo" src={process.env.PUBLIC_URL + '/logo.png'} />
+        <a href="/">Home Management Library Project</a>
+    </header>
+)
 
 export default Header
