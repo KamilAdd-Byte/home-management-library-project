a00edfa5de1357de1147c9b363db1075f018365a
MrMatikm <marczyk.mati@gmail.com>
2021-06-13 11:38:45 +0200

    Added javadocs

--- src/main/java/com/libraryproject/homemanagementlibraryproject/validation/BookValidator.java
+++ src/main/java/com/libraryproject/homemanagementlibraryproject/validation/BookValidator.java
@@ -4,5 +4,10 @@ import com.libraryproject.homemanagementlibraryproject.dto.BookDto;
 
 public interface BookValidator {
 
+    /**
+     * Checks if all the required fields are not null.
+     * @param book book to be checked
+     * @return false if any of the required fields is null; true otherwise
+     */
     boolean areAllRequiredFieldsNotNull(BookDto book);
 }
