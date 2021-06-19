5b92e69fec96f60739a8c9e4e7e3f88cd07fd62e
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-06-19 15:04:48 +0200

    Change link

--- src/main/java/com/libraryproject/homemanagementlibraryproject/restcontroller/BookRestController.java
+++ src/main/java/com/libraryproject/homemanagementlibraryproject/restcontroller/BookRestController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "https://tranquil-falls-99081.herokuapp.com")
+@CrossOrigin(origins = "*")
 public class BookRestController {
 
     @Autowired
