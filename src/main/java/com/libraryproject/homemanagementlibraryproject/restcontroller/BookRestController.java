2bf8ad5523e8ce1e86a37a79f082f12b878b0a43
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-06-23 22:49:48 +0200

    Change path on update

--- src/main/java/com/libraryproject/homemanagementlibraryproject/restcontroller/BookRestController.java
+++ src/main/java/com/libraryproject/homemanagementlibraryproject/restcontroller/BookRestController.java
@@ -66,7 +66,7 @@ public class BookRestController {
      * @param book book with changes
      * @return response entity with body with updated book
      */
-    @PutMapping(value = "/books/{id}",consumes = MediaType.APPLICATION_JSON_VALUE,
+    @PutMapping(value = "/book/{id}",consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<BookDto> updateBook (@PathVariable("id") Long id, @RequestBody BookDto book){
