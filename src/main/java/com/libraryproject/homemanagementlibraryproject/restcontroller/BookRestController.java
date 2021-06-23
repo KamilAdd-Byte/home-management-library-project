31b88c59f745451c2865590cf7116ac6d2f078c6
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-06-23 20:29:54 +0200

    Change put value update books to book

--- src/main/java/com/libraryproject/homemanagementlibraryproject/restcontroller/BookRestController.java
+++ src/main/java/com/libraryproject/homemanagementlibraryproject/restcontroller/BookRestController.java
@@ -66,7 +66,7 @@ public class BookRestController {
      * @param book book with changes
      * @return response entity with body with updated book
      */
-    @PutMapping(value = "books")
+    @PutMapping(value = "book")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<BookDto> updateBook (@RequestBody BookDto book){
         BookDto updateBook = bookService.updateBook(book);
